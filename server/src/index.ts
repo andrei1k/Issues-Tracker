021e4eb974cc5d4abf735461883ab5d97a488d8d
Andrey Kalchev <andreikalchev02@gmail.com>
2024-03-10 12:57:39 +0200

    project skelet

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import Knex from 'knex'
+import { config } from '../knexfile';
+import express from 'express'
+
+const knex = Knex(config.development)
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  knex('test').then(value => {
+    res.send(value)
+  }).finally(() => {
+    knex.destroy()
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

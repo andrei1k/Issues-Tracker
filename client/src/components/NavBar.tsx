7a9fc0ae945d01c17acbc1aab91699377c9e7145
Nikolay Dragostinov <nikolayddragostinov@gmail.com>
2024-03-24 15:56:38 +0200

    Fixed responsive nav-bar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,34 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './NavBar.css';
+
+// class NavBar extends Component {
+// items = [
+//     { label: 'Home', to: '/' },
+//     { label: 'About', to: '/about' },
+//     { label: 'Contact', to: '/contact' },
+//     { label: 'Login', to: '/login' },
+//     { label: 'Register', to:'/register'}
+// ];
+
+//     render() {
+//         return (
+//             <nav className={"navBar"}>
+//                 <button className="menu-button">Menu</button>
+//                 <div className="navigation">
+//                     {this.items.map((item, index) => (
+//                         <a key={index} className="menu-item" href={item.to}>
+//                             {item.label}
+//                         </a>
+//                     ))}
+//                 </div>
+//             </nav>
+//         );
+//     }
+// }
 
 interface NavItem {
     label: string;
     to: string;
-  }
-  
-class NavBar extends Component<{}, NavItem[]> {
-    state = {
-        items: [
+}
+
+const NavBar: React.FC = () => {
+    const [menuVisible, setMenuVisible] = useState<boolean>(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuVisible(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+    const toggleMenu = () => {
+        setMenuVisible(prevMenuVisible => !prevMenuVisible);
+    };
+
+    const items: NavItem[] = [
         { label: 'Home', to: '/' },
         { label: 'About', to: '/about' },
         { label: 'Contact', to: '/contact' },
         { label: 'Login', to: '/login' },
         { label: 'Register', to:'/register'}
-        ]
-    };
+    ];
 
-    render() {
-        return (
-        <nav className="navigation">
-            {this.state.items.map((item) => (
-                <a className="menu-item" href={item.to}>
-                    {item.label}
-                </a>
-
-            ))}
+    return (
+        <nav className={"navBar"}>
+            <button ref={menuRef} className="menu-button" onClick={toggleMenu}>Menu</button>
+            <div  className={`navigation ${menuVisible ? 'open' : ''}`}>
+                {items.map((item, index) => (
+                    <a key={index} className="menu-item" href={item.to}>
+                        {item.label}
+                    </a>
+                ))}
+            </div>
         </nav>
-        );
-    }
-}
-  
+    );
+};
+
 export default NavBar;

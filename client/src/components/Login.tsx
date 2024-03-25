0136297e685d37ee26a94e5f3daf60f7b2157d92
Nikolay Dragostinov <nikolayddragostinov@gmail.com>
2024-03-25 11:48:31 +0200

    Cleaned code

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,54 +1,7 @@
-// import React, { Component } from 'react';
-// import './Login.css';
-
-// class Login extends Component {
-//     inputText(event) {
-//         const input = event.target;
-//         input.placeholder = '';
-//     }
-
-//     inputFocus(event) {
-//         const input = event.target;
-//         if (!input.value) {
-//             if (input.id === 'email') {
-//                 input.placeholder = 'Enter email:';
-//             } else if (input.id === 'password') {
-//                 input.placeholder = 'Enter password:';
-//             }
-//         }
-//     }
-
-//     handleSubmit(event) {
-//         event.preventDefault();
-
-//     }
-
-//     render() {
-//         return (
-//             <div className='login-container'>
-//                 <form className='login-form' onSubmit={this.handleSubmit}>
-//                     <h2>Login</h2>
-//                     <div className='input-group'>
-//                         <input type='text' id='email' name='email' placeholder='Enter email:' 
-//                         onClick={this.inputText} onBlur={this.inputFocus} required></input>
-//                         <input type='password' id='password' name='password' placeholder='Enter password:' 
-//                         onClick={this.inputText} onBlur={this.inputFocus} required></input>
-//                     </div>
-//                     <div className='input-group button-container'>
-//                         <button type='submit'>Login</button>
-//                     </div>
-//                 </form>
-//             </div>
-//         );
-//     }
-// }
-
-// export default Login;
-
 import React from 'react';
 import './Login.css';
 
-const Login = () => {
+function Login() {
     const inputText = (event) => {
         const input = event.target;
         input.placeholder = '';
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,121 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 
-interface RegisterState {
-  firstName: string,
-  lastName: string,
-  email: string,
-  password: string;
-  confirmPassword: string;
-  passwordsMatch: boolean;
-}
+function Register() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordsMatch, setPasswordsMatch] = useState(true);
 
-class Register extends Component<{}, RegisterState> {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      passwordsMatch: true
+    const inputText = (event) => {
+        const input = event.target;
+        input.placeholder = '';
     };
 
-    // this.inputText = this.inputText.bind(this);
-    // this.inputFocus = this.inputFocus.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    const inputFocus = (event) => {
+        const input = event.target;
+        if (!input.value) {
+            if (input.id === 'first-name') {
+                input.placeholder = 'Enter first name:';
+            } else if (input.id === 'last-name') {
+                input.placeholder = 'Enter last name:';
+            } else if (input.id === 'email') {
+                input.placeholder = 'Enter email:';
+            } else if (input.id === 'password') {
+                input.placeholder = 'Enter password:';
+            } else if (input.id === 'confirm-password') {
+                input.placeholder = 'Confirm password:';
+            }
+        }
+    };
 
-  inputText(event) {
-      const input = event.target;
-      input.placeholder = '';
-  }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
 
-  inputFocus(event) {
-      const input = event.target;
-      if (!input.value) {
-          if (input.id === 'first-name') {
-            input.placeholder = 'Enter first name:';
-          } else if (input.id === 'last-name') {
-            input.placeholder = 'Enter last name:';
-          } else if (input.id === 'email') {
-            input.placeholder = 'Enter email:';
-          } else if (input.id === 'password') {
-            input.placeholder = 'Enter password:';
-          } else if (input.id === 'confirm-password') {
-            input.placeholder = 'Confirm password:';
-          }
-      }
-  }
+    const handleConfirmPasswordChange = (event) => {
+        const confirmPassword = event.target.value;
+        setConfirmPassword(confirmPassword);
+        setPasswordsMatch(confirmPassword === password);
+        
+        if (password === '') {
+            setPasswordsMatch(true); 
+        }
+    };
 
-  handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const dataE = { firstName, lastName, email };
+        fetch('http://10.108.4.127:3001/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataE)      
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Callback from server:', data);
+        })
+        .catch(error => {
+            console.error('Error sending query:', error);
+        });
+    };
 
-  handleConfirmPasswordChange(event) {
-    const confirmPassword = event.target.value;
-    this.setState((prevState) => ({
-      confirmPassword,
-      passwordsMatch: confirmPassword === prevState.password,
-    }));
-
-    if (this.state.password === '') {
-      this.setState({passwordsMatch: true}); 
-    }
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const firstName = event.target.elements['first-name'].value;
-    const lastName = event.target.elements['last-name'].value;
-    const email = event.target.elements['email'].value;
-    // console.log({ firstName, lastName, email });
-
-    const dataE = { firstName, lastName, email };
-    fetch('http://10.108.4.127:3001/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dataE)      
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Callback from server:', data);
-    })
-    .catch(error => {
-      console.error('Error sending query:', error);
-    });
-  }
-    render() {
-      return (
+    return (
         <div className='register-container'>
-                <form className='register-form' onSubmit={this.handleSubmit}>
-                    <h2>Register</h2>
-                    <div className='input-group'>
-                      <input type='text' id='first-name' name='first-name' placeholder='Enter first name:' 
-                          onClick={this.inputText} onBlur={this.inputFocus} required></input>
-                      <input type='text' id='last-name' name='last-name' placeholder='Enter last name:' 
-                          onClick={this.inputText} onBlur={this.inputFocus} required></input>
-                      <input type='text' id='email' name='email' placeholder='Enter email:' 
-                          onClick={this.inputText} onBlur={this.inputFocus} required></input>
-                      <input type='password' id='password' name='password' placeholder='Enter password:' 
-                          onClick={this.inputText} onBlur={this.inputFocus} onChange={this.handlePasswordChange} required></input>
-                      <input type='password' id='confirm-password' name='confirm-password' placeholder='Confirm password:' 
-                          onClick={this.inputText} onBlur={this.inputFocus} onChange={this.handleConfirmPasswordChange} required></input>
-                      {!this.state.passwordsMatch && <p id="p-match">Passwords do not match</p>}
-                    </div>
-                    <div className='input-group button-container'>
-                        <button type='submit'>Register</button>
-                    </div>
-                </form>
+            <form className='register-form' onSubmit={handleSubmit}>
+                <h2>Register</h2>
+                <div className='input-group'>
+                    <input type='text' id='first-name' name='first-name' placeholder='Enter first name:' 
+                        onClick={inputText} onBlur={inputFocus} required value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    <input type='text' id='last-name' name='last-name' placeholder='Enter last name:' 
+                        onClick={inputText} onBlur={inputFocus} required value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    <input type='text' id='email' name='email' placeholder='Enter email:' 
+                        onClick={inputText} onBlur={inputFocus} required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type='password' id='password' name='password' placeholder='Enter password:' 
+                        onClick={inputText} onBlur={inputFocus} required value={password} onChange={handlePasswordChange} />
+                    <input type='password' id='confirm-password' name='confirm-password' placeholder='Confirm password:' 
+                        onClick={inputText} onBlur={inputFocus} required value={confirmPassword} onChange={handleConfirmPasswordChange} />
+                    {!passwordsMatch && <p id="p-match">Passwords do not match</p>}
+                </div>
+                <div className='input-group button-container'>
+                    <button type='submit'>Register</button>
+                </div>
+            </form>
         </div>
-        );
-    }
-}
+    );
+};
 
 export default Register;

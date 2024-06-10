97158d8eeb51446134a078dcb137ba3668929b69
Nikolay Dragostinov <nikolayddragostinov@gmail.com>
2024-06-10 19:48:51 +0300

    added projects link

--- client/src/routes/PrivateOutlet.tsx
+++ client/src/routes/PrivateOutlet.tsx
@@ -18,25 +18,32 @@ function matchUserId(userId: number) {
   // const matchDashboard = window.location.pathname.match(/^\/dashboard\/(\d+)$/);
   const matchDashboard = window.location.pathname.match(/^\/(\d+)\/dashboard$/);
   const matchProject = window.location.pathname.match(/^\/\d+\/projects\/([^/]+)$/);
+  
   const matchOther = window.location.pathname === '/home' ||
                   window.location.pathname === '/' || 
                   window.location.pathname === '/issues' || 
                   window.location.pathname === '/add-issue' || 
                   window.location.pathname === '/workflow' || 
-                  window.location.pathname === '/profile';
+                  window.location.pathname === '/profile' ||
+                  matchProject;
 
   if (matchDashboard) {
     const urlUserId = parseInt(matchDashboard[1]);
+    console.log(urlUserId);
     return userId === urlUserId;
+
   } 
-  else if (matchProject) {
-    const urlUserId = parseInt(matchProject[0]);
-    return userId === urlUserId;
-  } 
+  // else if (matchProject) {
+  //   console.log(matchProject[0]);
+  //   const urlUserId = parseInt(matchProject[1]);
+  //   return userId === urlUserId;
+  // } 
   else if (matchOther) {
     return true; 
   }
-  return false;
+  else {
+    return false;
+  }
 }
 
 export default PrivateOutlet;

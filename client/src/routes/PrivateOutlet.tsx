fbea7d58a7b75d6ba3f6c02707997a5875eeee1b
Nikolay Dragostinov <nikolayddragostinov@gmail.com>
2024-06-12 11:35:02 +0300

    changed validation for url

--- client/src/routes/PrivateOutlet.tsx
+++ client/src/routes/PrivateOutlet.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Outlet, Navigate} from 'react-router-dom';
 import { getProjectInfo, getToken, getUserId, getUserInfo } from '../utils/Data.tsx';
-import { Project } from '../../../server/src/models/Project.ts';
 
 function PrivateOutlet () {
     const token = getToken();
@@ -16,31 +15,21 @@ function PrivateOutlet () {
 }
 
 function matchUrl(userId: number, projectId: number) {
-  const matchDashboard = window.location.pathname.match(/^\/(\d+)\/dashboard$/);
-  const matchProject = window.location.pathname.match(/^\/\d+\/projects\/(\d+)$/);
-  const matchAddIssue = window.location.pathname.match(/^\/(\d+)\/projects\/(\d+)\/add-issue$/);
+  const pathParts = window.location.pathname.split('/');
 
-  const matchOther = window.location.pathname === '/home' ||
-                  window.location.pathname === '/' || 
-                  window.location.pathname === '/issues' || 
-                  window.location.pathname === '/workflow' || 
-                  window.location.pathname === '/profile' ||
-                  matchProject || matchAddIssue;
-
-  if (matchDashboard) {
-    const urlUserId = parseInt(matchDashboard[1]);
-    return userId === urlUserId;
-  } 
-  // else if (matchProject) {
-  //   console.log(matchProject);
-  //   const urlUserId = parseInt(matchProject[0]);
-  //   // console.log("url user id " + urlUserId);
-  //   const urlProjectId = parseInt(matchProject[1]);
-  //   // console.log("url proj id " + urlProjectId);
-  //   return userId === urlUserId && projectId === urlProjectId;
-  // } 
-  else if (matchOther) {
-    return true; 
+  if (pathParts[2] === 'dashboard') {
+    return Number(pathParts[1]) === userId;
+  }
+  else if (pathParts[2] === 'projects') {
+    return Number(pathParts[1]) === Number(userId) &&
+          Number(pathParts[3]) === Number(projectId);
+  }
+  else if (pathParts[1] === 'home' || 
+          pathParts[1] === 'workflow' ||
+          pathParts[1] === 'profile' || 
+          pathParts[1] === '') 
+  {
+    return true;
   }
   else {
     return false;

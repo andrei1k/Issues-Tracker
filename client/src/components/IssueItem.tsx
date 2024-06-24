d3d293fd2a085fe9ab48c2f67afda6b800f2c826
Хилда Митева <mitewwaa@gmail.com>
2024-06-24 21:09:35 +0300

    changed styles

--- client/src/components/IssueItem.tsx
+++ client/src/components/IssueItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaTrashCan, FaPen } from "react-icons/fa6";
-import '../styles/IssueList.css';
+import "../styles/IssueList.css";
 import Modal from "./Modal.tsx";
 import EditIssue from "./EditIssue.tsx";
 import { Issue } from "../services/IssueService.ts";
@@ -12,27 +12,25 @@ interface Props {
 }
 
 function IssueItem({ issue, removeIssue, viewIssues }: Props) {
-
   const handleClickRemove = (e) => {
     e.preventDefault();
     if (issue.id) {
       removeIssue(issue.id);
     }
-
-  }
+  };
 
   const getPriorityText = (priority: number) => {
     switch (priority) {
       case 1:
-        return 'High';
+        return "High";
       case 2:
-        return 'Medium';
+        return "Medium";
       case 3:
-        return 'Low';
+        return "Low";
       default:
-        return 'Unknown';
+        return "Unknown";
     }
-  }
+  };
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
@@ -47,39 +45,52 @@ function IssueItem({ issue, removeIssue, viewIssues }: Props) {
   const handleEditBtn = (e) => {
     e.preventDefault();
     openModal();
-  }
-  
+  };
+
   return (
-    <> 
-    <li className="issue-item">
-      <ul className="issues">
-        <li className="issue-field">
-          <span className="field-label">Title:</span>
-          <span className="field-value">{issue.title}</span>
-        </li>
-        <li className="issue-field">
-          <span className="field-label">Description:</span>
-          <span className="field-value">{issue.description}</span>
-        </li>
-        <li className="issue-field">
-          <span className="field-label">Priority:</span>
-          <span className="field-value">{getPriorityText(issue.priority)}</span>
-        </li>
-        <li className="issue-field">
-          <span className="field-label">Assigned To:</span>
-          <span className="field-value">{issue.assignedTo}</span>
-        </li>
-        <li className="button-field">
-          <FaPen data-issueid={issue.id} onClick={handleEditBtn}/>
-          <Modal 
-            isOpen={isModalOpen} 
-            onClose={closeModal} 
-            children={<EditIssue issueId={issue.id} closeModal={closeModal} viewIssues={viewIssues}/>}>
-          </Modal>
-          <FaTrashCan onClick={handleClickRemove}/>
-        </li>
-      </ul>
-    </li>
+    <>
+      <li className="issue-item">
+        <ul className="issues">
+          <div>
+            <li className="issue-field">
+              <span className="field-label">Title:</span>
+              <span className="field-value">{issue.title}</span>
+            </li>
+          </div>
+          <li className="issue-field">
+            <span className="field-label">Priority:</span>
+            <span className="field-value">
+              {getPriorityText(issue.priority)}
+            </span>
+          </li>
+          <li className="issue-field">
+            <span className="field-label">Assigned To:</span>
+            <span className="field-value">{issue.assignedTo}</span>
+          </li>
+          <div className="info-description">
+            <li className="issue-field">
+              <span className="field-label">Description:</span>
+              <span className="field-value">{issue.description}</span>
+            </li>
+          </div>
+
+          <li className="button-field">
+            <FaPen data-issueid={issue.id} onClick={handleEditBtn} />
+            <Modal
+              isOpen={isModalOpen}
+              onClose={closeModal}
+              children={
+                <EditIssue
+                  issueId={issue.id}
+                  closeModal={closeModal}
+                  viewIssues={viewIssues}
+                />
+              }
+            ></Modal>
+            <FaTrashCan onClick={handleClickRemove} />
+          </li>
+        </ul>
+      </li>
     </>
   );
 }
